805c428052ab6dd758c602fa595385a7657a2554
Jakub Kowalski <258959@student.pwr.edu.pl>
2021-03-04 18:20:33 +0100

    Dodano zmienne zewnetrzne spoza program.c

--- test3/modul.c
+++ test3/modul.c
@@ -3,5 +3,5 @@
  *  Zestaw wazniejszych stalych
  */
 
-const double PI = 3.141593;
-const double E  = 2.718282;
+extern const double PI = 3.141593;  /* dolaczenie zmiennych spoza funkcji z program.c */
+extern const double E  = 2.718282;
